ec4b19409dd6d959ed0ec98748e13423709e7478
Nina Palma <ninapalmadev@gmail.com>
2022-09-19 15:54:44 -0300

    loop for com array

---
+++ src/br/com/dio/repeticoes/ForArrays.java
@@ -0,0 +1,12 @@
+package br.com.dio.repeticoes;
+
+public class ForArrays {
+    public static void main(String[] args) {
+        String cantora[]= {"MADONNA", "BEYONCE", "KALYE", "HAYLEY"};
+
+        for(int i = 0; i < cantora.length; i++){
+            System.out.println("A cantora no índice i= " + i + " é " + cantora[i]);
+        }
+
+    }
+}
